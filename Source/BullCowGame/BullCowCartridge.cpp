deea1e3dc8b89b0af210487e07338cfed51cc086
Henry-Ning <henryhuning@gmail.com>
2020-07-04 16:44:57 -0700

    45. Using The In Game Terminal 9min

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine("Hi There!");
+    PrintLine("Input something and press enter");
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+}
