a08a3b1ab4075949d4b6006880509ba907e4ee57
Henry-Ning <henryhuning@gmail.com>
2020-07-05 14:15:59 -0700

    51. Member Variables 6min

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,15 +4,16 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Hi There!"));
+    PrintLine(TEXT("Hi Henry!"));
     PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number!
     PrintLine(TEXT("Input something and press enter"));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake"); // Move outside this function.
+    
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("You win"));
